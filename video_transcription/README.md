998398186cbf29c6a940907885dcb3050581ad97
TomKaminski <tom.kaminski@thingsthinking.net>
2023-04-30 21:04:56 +0200

    Fix hardcoded filename left in run.py and update README.md

--- video_transcription/README.md
+++ video_transcription/README.md
@@ -1,5 +1,5 @@
 # Dependencies
-The transcription dependencies are separate from the rest. (For good reason)
+The transcription dependencies are separate from the rest. (For good reason) <br>
 Create a new virtual environment and install the dependencies:
 
 ```
@@ -8,27 +8,22 @@ Create a new virtual environment and install the dependencies:
     pip install -r requirements.txt
 ```
 
-You'll also need the command-line tool ffmpeg installed.
+You'll also need the command-line tool ffmpeg installed. <br>
 Use a package manager or install from source.
 
 # Downloading the playlist
-To download the videos, run the following command:
-
 ```
-    yt-dlp -o "video_transcription/data/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s"
-        --write-info-json --clean-info-json --write-playlist-metafiles --encoding "utf-8"
-        --extract-audio --audio-format mp3 --audio-quality 0
-        "https://www.youtube.com/playlist?list=PL45xb3ujEhqUexNt53jb9WT2mS-uUaUrn"
+    yt-dlp -o "video_transcription/data/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" --write-info-json --clean-info-json --write-playlist-metafiles --encoding "utf-8" --extract-audio --audio-format mp3 --audio-quality 0 YOUR_PLAYLIST_URL
 ```
 
-You can test it out with (this playlist)[https://www.youtube.com/playlist?list=PL45xb3ujEhqUexNt53jb9WT2mS-uUaUrn].
+This will download all the necessary data into the `video_transcription/data/PLAYLIST_NAME` directory. <br>
+<br>
+You can test it out with [this playlist](https://www.youtube.com/playlist?list=PL45xb3ujEhqUexNt53jb9WT2mS-uUaUrn). <br>
 Take a break, watch one of the videos. I think they're funny.
 
 # Transcribe the playlist
-To test it out, run the following command:
-
 ```
-    python -m video_transcription.run --playlist-directory "transcription/data/PLAYLIST_NAME"
+    python -m video_transcription.run --playlist-directory "video_transcription/data/PLAYLIST_NAME"
 ```
 
 The result should be a file called `transcription/data/PLAYLIST_NAME/PLAYLIST_NAME.xlsx`.
--- video_transcription/run.py
+++ video_transcription/run.py
@@ -9,18 +9,20 @@ import whisper
 
 
 class Playlist:
-    def __init__(self, playlist_directory, playlist_name):
-        playlist_info = json.load(open(os.path.join(playlist_directory, f"000 - {playlist_name}.info.json")))
+    def __init__(self, playlist_path):
+        playlist_directory = sorted(os.listdir(playlist_path))
+        playlist_info = json.load(open(os.path.join(playlist_path, playlist_directory[0]), encoding="utf-8"))
+
         self.title = playlist_info["title"]
         self.description = playlist_info["description"]
         self.tags = playlist_info["tags"]
 
         self.videos = []
-        videos = os.listdir(playlist_directory)[1:]
+        videos = playlist_directory[1:]
         for info, mp3 in zip(videos[::2], videos[1::2]):
-            video_info = json.load(open(os.path.join(playlist_directory, info), encoding="utf-8"))
+            video_info = json.load(open(os.path.join(playlist_path, info), encoding="utf-8"))
             self.videos.append({
-                "mp3": str(os.path.join(playlist_directory, mp3)),
+                "mp3": str(os.path.join(playlist_path, mp3)),
                 "title": video_info["title"],
                 "description": video_info["description"],
                 "tags": video_info["tags"],
@@ -31,6 +33,9 @@ class Playlist:
     def __iter__(self):
         return iter(self.videos)
 
+    def __len__(self):
+        return len(self.videos)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -38,6 +43,9 @@ if __name__ == '__main__':
     parser.add_argument('--playlist-directory', type=str, default='.')
     parser.add_argument('--window-size', type=int, default=6)
     args = parser.parse_args()
+
+    # Load playlist
+    playlist = Playlist(args.playlist_directory)
 
     # Load model
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -47,7 +55,7 @@ if __name__ == '__main__':
 
     # Process playlist
     library = []
-    for video in tqdm.tqdm(Playlist(args.playlist_directory, "Korrekte Aussprache - Lektion 1")):
+    for video in tqdm.tqdm(playlist, total=len(playlist)):
         # One entry for the whole transcript
         transcript = model.transcribe(audio=video["mp3"])
         library.append({
@@ -61,9 +69,9 @@ if __name__ == '__main__':
             "Tags": ["TRANSCRIPT", video["playlist"]]
         })
 
-        # Followed by an entry for all args.window consecutive segments
+        # Followed by an entry for all args.window_size consecutive segments
         for start in range(len(transcript["segments"])):
-            end = min(len(transcript["segments"]) - 1, start + args.window)
+            end = min(len(transcript["segments"]) - 1, start + args.window_size)
             library.append({
                 'Name': video["title"],
                 "Content": " ".join(map(lambda s: s["text"], transcript["segments"][start:end + 1])),
@@ -75,5 +83,5 @@ if __name__ == '__main__':
                 "Tags": ["SEGMENT", video["playlist"]]
             })
 
-    pd.DataFrame(library).to_excel(f"{os.path.join(args.playlist_directory, os.path.basename(args.playlist_directory))}.xlsx")
-    print(f"Wrote output to {os.path.join(args.playlist_directory, os.path.basename(args.playlist_directory))}.xlsx")
+    pd.DataFrame(library).to_excel(f"{os.path.join(args.playlist_directory, 'semantha_library')}.xlsx")
+    print(f"Wrote output to {os.path.join(args.playlist_directory, 'semantha_library')}.xlsx")
